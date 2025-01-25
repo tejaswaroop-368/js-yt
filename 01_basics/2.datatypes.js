75edbb8342931d9794b7c724bf63b049bdfa3bd8
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-25 18:04:09 +0000

    datatypes

---
+++ 01_basics/2.datatypes.js
@@ -0,0 +1,21 @@
+"use strict"// treat all js code as newer version
+
+// alert("hello") we are using node js not browser
+
+console.log(3+3) //code readabiility should be high
+
+let name= "hitesh"
+let n=1
+let y=true
+ 
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol = unique
+
+// object
+console.log(typeof "tehads");
+console.log(typeof null)
+console.log(typeof undefined)
