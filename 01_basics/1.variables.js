eddf36ae4d498ea1942debb0bb25cefc1a806703
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-25 18:05:27 +0000

    variables

---
+++ 01_basics/1.variables.js
@@ -0,0 +1,20 @@
+const accId = 1453;
+let accEmail="hitehs@google.com"
+var accpass="1234"
+accountCIty="jaipur"
+
+// accId= 2 not alowed
+
+console.log(accId);
+
+accEmail="teja@gnmail.com"
+accpass="sdkf2"
+accountCIty="vslp"
+console.log(accEmail);
+console.log(accpass);
+console.log(accountCIty);
+console.table([accEmail,accId,accountCIty,accpass])
+
+/* prefer not to use var because of issue in 
+block  and functional scope */
+
