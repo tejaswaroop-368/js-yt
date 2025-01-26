09e0edb94da300253cec2af4ff0458f76dacd780
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-26 07:27:15 +0000

    conversions

--- 01_basics/3.conversionOperations.js
+++ 01_basics/3.conversionOperations.js
@@ -1,8 +1,26 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+// operations
+
+let value = 3
+let negvalue=-value
+console.log(negvalue)
+
+let str = "string"
+let str2="teja"
+
+let str3= str + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log(2+1+"2")
