9b5a4685c7a2e2440c92368e857913afcc0e610b
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-26 07:41:54 +0000

    datatypes summary

---
+++ 5.datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitive
+
+// 7 types:
+/* string
+    number
+    boolean 
+    null 
+    undefined
+    symbol
+    bigint
+    
+    
+    
+    Reference types (non primitve)
+    Array 
+    objects
+    functions
+
+    */
