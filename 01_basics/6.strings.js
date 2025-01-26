8ed968d4e4c92eabd91bf267d6c0dfef958f0797
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-26 10:34:57 +0000

    datatypes,strings

---
+++ 01_basics/6.strings.js
@@ -0,0 +1,12 @@
+const name = "hitesh"
+const repocount = 50
+
+console.log(name + repocount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+const gname="tejaswarop"
+
+console.log(gname.__proto__);
+
+console.log(gname.toUpperCase())
