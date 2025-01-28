6327da4eee6cbb884ca8eac712c38fc5d1c7fb53
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-01-28 17:02:35 +0000

    math and num

---
+++ 01_basics/7.num,maths.js
@@ -0,0 +1,25 @@
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(2))
+
+const hund=1000000
+console.log(hund.toLocaleString('en-IN'))
+
+// maths now *********************************************
+
+
+
+// so manyu values in math functions like pi and e
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.3))
+console.log(Math.floor(3.4))
+console.log(Math.ceil(3.4))
+// min,max,random
+
+console.log((Math.random()*10)+1);
+
