0175c68d7132591b98be895cdc356cfdc8e417ab
tejaswaroop-368 <pallitejaswaroop@gmail.com>
2025-02-01 07:41:00 +0000

    control flow

--- 02.basics/4.controlflow.js
+++ 02.basics/4.controlflow.js
@@ -2,3 +2,21 @@ if(2==2){
     console.log("executed");
 }
 // <,>,<=,>=,==,!=,===,!===
+
+//for of
+
+const arr= [1,2,3,4,5]
+
+for(const num of arr){
+
+    console.log(num)
+}
+
+//maps
+// collection of key vlue pairs
+
+const map = new Map()
+map.set('in',"india")
+map.set('in',"india")
+map.set('in',"india")
+
